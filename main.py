fc735c6e0d346427c4496fe8b208d371cf63f906
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2021-04-01 12:28:45 +0100

    Made Tiles be rotated by clicking and bug fixes
    -When the Screen and the window doesn't have the same dimension there may be some scalar mix up which cause the mouse to not press the right square. to fix this I made a window to Screen Scalar
    - I also made Tile be rotated by clicking on them.
    - And some recoding which is too many to state

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from math import floor
 from pathlib import Path
 from pygame.locals import(
     QUIT,
+    MOUSEBUTTONDOWN,
+    MOUSEBUTTONUP,
 )
 
-class Tile(object):
-    def __init__(self, atype, position, RGB, Rotation=0):
-        self.type = atype
+class Tile(object): 
+    def __init__(self, FORMAT, position, RGB=(False, False, False), rotationPos=0):
+        self.atype = FORMAT.split('_')[1]
+        self.parent = FORMAT.split('_')[0]
+        self.position = tuple(floor(Tile.TileSizeScalar*x + Tile.TileSizeScalar/2) for x in position)
+        self.RGB = RGB
+        self.rotationPos = rotationPos
+        self.image = pygame.transform.scale(getImage(self.parent, self.atype), (Tile.TileSizeScalar, Tile.TileSizeScalar))
+        self.rect = self.image.get_rect(center=self.position)
+        print (self.position)
+    def Clicked(self):
+        self.rotationPos = (self.rotationPos + 1) %4
+        # ((this_month - 1) % 12 + 12) % 12
 
 
 # Function
@@ -34,41 +46,63 @@ def loadImagesWithName():
 def readMapFile(fileName='map'):
     global BOARD, TILECOUNT
     with open(f'{fileName}.txt', 'r') as MapData:
-        for row in enumerate(MapData):
-            Value = [Tile.rstrip() for Tile in row[1].split(',')]
-            BOARD[row[0]] = Value
-    TILECOUNT = len(BOARD)  # Used to check which dimesion is the most
+        for yindex, row in enumerate(MapData):
+            BOARD[yindex] = [0]*len(row.split(','))
+    TILECOUNT = len(BOARD)  # Used to check which dimension is the most
     for i in BOARD.values():
         if len(i) > TILECOUNT:
             TILECOUNT = len(i)
+    Tile.TileSizeScalar = floor(min(SCREEN_WIDTH, SCREEN_HEIGHT)/TILECOUNT)
+    with open(f'{fileName}.txt', 'r') as MapData:
+        for yindex, row in enumerate(MapData):
+            Value = [Tile(tile.strip(), (xindex, yindex))  for xindex, tile in enumerate(row.split(','))]
+            BOARD[yindex] = Value
 
 
 def DisplayBoard():
-    global BOARD, TILECOUNT
-    TileSize = floor(min(SCREEN_WIDTH, SCREEN_HEIGHT)/TILECOUNT)
+    global BOARD
     for row in BOARD:
-        for item in enumerate(BOARD[row]):
+        for index, tile in enumerate(BOARD[row]):
             Image = pygame.transform.scale(
-                getImage(item[1]), (TileSize, TileSize))
-            SCREEN.blit(Image, (item[0]*TileSize, row*TileSize))
+                getImage(tile.parent, tile.atype), (Tile.TileSizeScalar, Tile.TileSizeScalar))
+            # SCREEN.blit(Image, tuple(TileSize*x for x in tile.position))
+            RotationBlit(SCREEN, tile.image, tile.rotationPos*90, tile.position)
 
 
-def getImage(TileName):
-    TileName = [tile.lstrip() for tile in TileName.split('_')]
-    if TileName[0] == 'Paths':
+def getImage(parent, atype):
+    if parent == 'Paths':
         global PathsImageDict
-        return PathsImageDict[TileName[1]]
-    elif TileName[0] == 'Nodes':
+        return PathsImageDict[atype]
+    elif parent == 'Nodes':
         global NodesImageDict
-        return NodesImageDict[TileName[1]]
+        return NodesImageDict[atype]
     else:
         global FountainImageDict
-        return FountainImageDict[TileName[1]]
+        return FountainImageDict[atype]
+
+
+def mouseCollision(mousePos):
+    global WindowToScreenScalar
+    mousePos = tuple(pos*wintoscr for pos, wintoscr in zip(mousePos, WindowToScreenScalar))
+    global BOARD
+    for row in BOARD.values():
+        for tile in row:
+            if tile.rect.collidepoint(mousePos):
+                tile.Clicked()
+
+
 
 
 # used to simmulate mixing Colors
 COLORKEY = {0: (192, 192, 192), 1: (255, 0, 0), 2: (0, 157, 255), 3: (144, 0, 255),
             4: (255, 196, 0), 5: (255, 115, 0), 6: (0, 255, 17), 7: (255, 255, 255)}
+
+
+def RotationBlit(Surface, Image, Angle, Position, Alpha=255):
+    rotatedIMG = pygame.transform.rotate(Image, Angle)
+    rotatedIMG.set_alpha(Alpha)
+    Surface.blit(rotatedIMG, (int(Position[0] - rotatedIMG.get_width()/2),
+                              int(Position[1] - rotatedIMG.get_height()/2)))
 
 
 def ColorMixer(RGB):
@@ -83,18 +117,20 @@ def ColorMixer(RGB):
 
 
 # Globals
-global NodesImageDict, PathsImageDict, FountainImageDict, BOARD, TILECOUNT, FPS, SCREEN_HEIGHT, SCREEN_WIDTH, WINDOW_SIZE, SCREEN, DISPLAY
+global NodesImageDict, PathsImageDict, FountainImageDict, BOARD, TILECOUNT, FPS, SCREEN_HEIGHT, SCREEN_WIDTH, WINDOW_SIZE, SCREEN, DISPLAY, WindowToScreenScalar
 TILEIMAGESIZE = 20
 WINDOW_SIZE = (500, 500)
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 2000
+SCREEN_HEIGHT = 2000
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 FPS = 60
 TILECOUNT = 0
 NodesImageDict = {}
 PathsImageDict = {}
 FountainImageDict = {}
 BOARD = {}
-
+WindowToScreenScalar = tuple(scr/win for win, scr in zip(WINDOW_SIZE, SCREEN_SIZE))
+print (WindowToScreenScalar)
 
 # Window
 pygame.display.set_caption("Template")
@@ -113,6 +149,9 @@ def Game():
         for event in pygame.event.get():
             if event.type == QUIT:
                 PROGRAM_RUNNING = False
+            elif event.type == MOUSEBUTTONDOWN:
+                mouseCollision(pygame.mouse.get_pos())
+                
 
         SCREEN.fill((255, 0, 0))
         DisplayBoard()
